444fc1414a9f5fe7067132c7df539a758db3e44d
Fredrick Galoso <fred@wayoutmind.com>
2011-03-09 23:34:49 -0600

    cleaned up directory layout and updated/refactored setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # encoding: utf-8
 from distutils.core import setup
- 
+
 setup(
-    name = 'seat',
-    version = '0.2.2',
-    description = 'Seat-Python is an elegant and lightweight REST interface to CouchDB, complete with ORM.',
-    author = 'Fredrick Galoso - Stackd, LLC',
-    license = 'MIT/X11',
-    url = 'https://github.com/stackd/seat-py',
-	download_url = 'https://github.com/stackd/seat-py',
-    package_dir = {'': 'src'},
-    py_modules = [
+    name='seat',
+    version='0.2.2',
+    description='Seat-Python is an elegant and lightweight REST interface to CouchDB, complete with ORM.',
+    author='Fredrick Galoso - Stackd, LLC',
+    license='MIT/X11',
+    url='https://github.com/stackd/seat-py',
+    download_url='https://github.com/stackd/seat-py',
+    package_dir={'': 'seat'},
+    py_modules=[
         'seat',
     ],
 )
