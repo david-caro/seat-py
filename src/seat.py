45331a0760a24d2544b29cec3d476ad1e6b5b011
Fredrick Galoso <fred@wayoutmind.com>
2011-03-09 20:15:44 -0600

    PEP 8 refactoring - http://www.python.org/dev/peps/pep-0008/

--- src/seat.py
+++ src/seat.py
@@ -30,14 +30,15 @@ import yaml
 
 __author__ = 'Fredrick Galoso'
 __version__ = '0.2.2'
-        
+
+
 class Seat(object):
-    
+
     HOST = 'localhost'
     PORT = '5984'
     USER_AGENT = 'Seat-Python (0.2.2)'
-    
-    def __init__(self, database = '', username = None, password = None):
+
+    def __init__(self, database='', username=None, password=None):
         if re.match(r'^http\://|^https://', database):
             uri = urlparse.urlparse(database)
             database = uri.path[1:]
@@ -46,37 +47,37 @@ class Seat(object):
             self.HOST = uri.hostname
             self.PORT = str(uri.port)
         if username == None and password == None:
-            self.resource = httplib.HTTPConnection(self.HOST+u':'+self.PORT)
-            self.headers = {'Content-Type' : 'application/json', 'User-Agent' : self.USER_AGENT}
+            self.resource = httplib.HTTPConnection(self.HOST + u':' + self.PORT)
+            self.headers = {'Content-Type': 'application/json', 'User-Agent': self.USER_AGENT}
         else:
-            self.resource = httplib.HTTPConnection(self.HOST+u':'+self.PORT)
-            self.headers = {'Content-Type' : 'application/json', 'User-Agent' : self.USER_AGENT, 'Authorization' : 'Basic '+string.strip(base64.encodestring(username+':'+password))}
+            self.resource = httplib.HTTPConnection(self.HOST + u':' + self.PORT)
+            self.headers = {'Content-Type': 'application/json', 'User-Agent': self.USER_AGENT, 'Authorization': 'Basic ' + string.strip(base64.encodestring(username + ':' + password))}
         self.database = database
-        
+
         self.Utils = Utils(self)
         self.Cache = Cache(self)
-            
+
     def __send(self, method, args):
         """Private class method to handle most HTTP requests."""
         if (args == None):
-            self.resource.request(method, u'/'+self.database, None, self.headers)
+            self.resource.request(method, u'/' + self.database, None, self.headers)
             request = self.resource.getresponse()
             return json.loads(request.read())
         elif (args != None):
-            self.resource.request(method, u'/'+self.database+u'/'+str(args), None, self.headers)
+            self.resource.request(method, u'/' + self.database + u'/' + str(args), None, self.headers)
             request = self.resource.getresponse()
             return json.loads(request.read())
-            
-    def get(self, doc = None):
+
+    def get(self, doc=None):
         """Given no arguments, will return status of the database as <type 'dict'>, else it will return a document given an _id.
         """
         return self.__send(u'GET', doc)
-        
-    def post(self, path = None):
+
+    def post(self, path=None):
         """Calls a variety of CouchDB functions such as _compact."""
         return self.__send(u'POST', path)
-        
-    def put(self, doc = None):
+
+    def put(self, doc=None):
         """Given no arguments, this method will create a new database as instantiated; else it is the primary method to update or create a document.
             Creating a new database:
                 db = Seat('new_database')
@@ -89,13 +90,13 @@ class Seat(object):
                 db.put(doc)
         """
         if type(doc).__name__ == 'dict':
-            self.resource.request(u'PUT', u'/'+self.database+u'/'+str(doc['_id']), json.dumps(doc), self.headers)
-            request = self.resource.getresponse() 
+            self.resource.request(u'PUT', u'/' + self.database + u'/' + str(doc['_id']), json.dumps(doc), self.headers)
+            request = self.resource.getresponse()
             return json.loads(request.read())
         else:
             return self.__send(u'PUT', doc)
-            
-    def delete(self, doc = None):
+
+    def delete(self, doc=None):
         """Given no arguments, this method will delete the entire database. Otherwise, it will delete a document given an _id and _rev.
             Deleting an entire database:
                 db = Seat('existing_database')
@@ -106,13 +107,13 @@ class Seat(object):
                 db.delete(doc)
         """
         if type(doc).__name__ == 'dict':
-            self.resource.request(u'DELETE', '/'+self.database+'/'+str(doc['_id'])+u'/?rev='+str(doc['_rev']), None, self.headers)
+            self.resource.request(u'DELETE', '/' + self.database + '/' + str(doc['_id']) + u'/?rev=' + str(doc['_rev']), None, self.headers)
             request = self.resource.getresponse()
             return json.loads(request.read())
         else:
             return self.__send(u'DELETE', doc)
-    
-    def view(self, ddoc, view, key = None, args = None):
+
+    def view(self, ddoc, view, key=None, args=None):
         """Returns view based on design document, view, and key.
             db = Seat('existing_database')
             db.view('user', 'by_first_name', 'Kenny')
@@ -121,17 +122,18 @@ class Seat(object):
             uri = '/%s/_design/%s/_view/%s?key=%s' % (self.database, ddoc, view, json.dumps(key))
         else:
             uri = '/%s/_design/%s/_view/%s' % (self.database, ddoc, view)
-            
+
         self.resource.request(u'GET', uri, None, self.headers)
         request = self.resource.getresponse()
         return json.loads(request.read())['rows']
-        
+
+
 class Utils(object):
     """Utilities for updating views and data validation."""
-    def __init__(self, seat, path = None):
+    def __init__(self, seat, path=None):
         self.seat = seat
         self.path = path
-        
+
     def push_views(self):
         if self.path == None:
             self.path = os.getcwd() + '/config/'
@@ -140,17 +142,19 @@ class Utils(object):
                 if yaml_file.find('.yaml'):
                     view = file(self.path + yaml_file, 'r')
                     print(yaml.load(view))
-        
+
+
 class Cache(object):
     """Redis caching layer."""
     def __init__(self, seat):
         self.seat = seat
     pass
-    
+
+
 class Object(dict):
     """Bare-metal ORM awesomeness.
             Create a new object model to be saved in CouchDB:
-        
+
             import seat
             class User(seat.Object):
                 database = seat.Seat('musicians')
@@ -166,12 +170,12 @@ class Object(dict):
         for key in kwargs:
             self[key] = kwargs[key]
         if '_id' not in kwargs:
-            self['_id'] = self.__class__.__name__ + '.' +hashlib.sha1(str(self)).hexdigest()
+            self['_id'] = self.__class__.__name__ + '.' + hashlib.sha1(str(self)).hexdigest()
             self.user_defined_id = False
         else:
             self['_id'] = kwargs['_id']
             self.user_defined_id = True
-        
+
     def __hash(self):
         """SHA1 checksum object contents."""
         contents = {}
@@ -179,15 +183,15 @@ class Object(dict):
             if key != '_id':
                 contents[key] = self[key]
         return hashlib.sha1(str(contents)).hexdigest()
-        
+
     def exists(self):
         response = self.database.get(self['_id'])
         if ('error' in response):
             return False
         else:
             return True
-            
-    def get(self, _id = None):
+
+    def get(self, _id=None):
         if _id != None:
             request = self.database.get(_id)
             doc = self.__class__()
@@ -200,19 +204,19 @@ class Object(dict):
                 raise SeatError(404, 'Document not found.')
             else:
                 return response
-                
+
     def update(self):
         response = self.database.get(self['_id'])
         self['_rev'] = response['_rev']
         return self.database.put(dict(self))
-    
+
     def save(self):
         response = self.database.put(dict(self))
         if ('error' in response and response['error'] == 'conflict' and not self.user_defined_id):
             #Verify that contents of the object have not changed
             self['_id'] = self.__class__.__name__ + '.' + self.__hash()
             return self.database.put(dict(self))
-            
+
     def delete(self):
         try:
             if (self.exists):
@@ -221,7 +225,8 @@ class Object(dict):
                 return response
         except KeyError:
             raise SeatError(404, 'Document not found.')
-        
+
+
 class SeatError(Exception):
     def __init__(self, type, message):
             Exception.__init__(self, message)
